e38196ea4a582c82997c86bbdff92a2afea8330e
AndrePerte <andrea.pertegato@studio.unibo.it>
2024-06-04 15:39:17 +0200

    Small adjustments

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container mx-4 px-2 border">
     <h3>ABOUT COMPONENT</h3>
-        <p>Sito internet fatto da Stefano al Fron e Andrea al Back</p>
+        <p>Sito internet realizzato da Stefano al FrontEnd e Andrea al BackEnd</p>
         <p>Ringraziamenti al Prof Delnevo</p>
     </div>
 </template>
