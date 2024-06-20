42ac42b072ea5f1051d5d94169e084505d4d804d
SantoSte2 <stefano.santoni@studio.unibo.it>
2024-06-20 14:50:34 +0200

    fix addCard component

--- src/components/CardLibri.vue
+++ src/components/CardLibri.vue
@@ -1,43 +1,50 @@
 <template>
 
-    <div class="d-flex flex-row mx-3 px-2">
-        <div class="card-group">
-            <div class="card" v-for="card in cardLibri" :key="card.id">
-                <img src="" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">{{ card.titoloCard }}</h5>
-                    <h5>{{ card.copieCard }}</h5>
-                    <p class="card-text">{{ card.descrizione }}</p>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-6">
-                                <button class="btn btn-primary" v-show="(card.copieCard)"
-                                    @click="prenotaCard(card.id)">PRENOTA</button>
+
+
+    <div class="col-12">
+        <div class="card-group mx-4 my-2">
+
+            <div class="row">
+                <div class="card col-3" v-for="card in cardLibri" :key="card.id">
+                    <img src="" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ card.titoloCard }}</h5>
+                        <h5>{{ card.copieCard }}</h5>
+                        <p class="card-text">{{ card.descrizione }}</p>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-6">
+                                    <button class="btn btn-primary" v-show="(card.copieCard)"
+                                        @click="prenotaCard(card.id)">PRENOTA</button>
+                                </div>
+                                <div class="col-6">
+                                    <button class="btn btn-warning" v-if="(card.copieCard) <= 0"
+                                        @click="metticodaCard">CODA</button>
+                                </div>
+
                             </div>
-                            <div class="col-6">
-                                <button class="btn btn-warning" v-if="(card.copieCard) <= 0"
-                                    @click="metticodaCard">CODA</button>
+                            <div class="row">
+                                <div class="col-6">
+                                    <button class="btn btn-primary" @click="addcopieCard(card.id)">RESTITUISCI</button>
+                                </div>
+                                <div class="col-6">
+                                    <button class="btn btn-primary" @click="cancellaCard(card.id)">CANCELLA</button>
+                                </div>
                             </div>
+
+
+
 
                         </div>
-                        <div class="row">
-                            <div class="col-6">
-                                <button class="btn btn-primary" @click="addcopieCard(card.id)">RESTITUISCI</button>
-                            </div>
-                            <div class="col-6">
-                                <button class="btn btn-primary" @click="cancellaCard(card.id)">CANCELLA</button>
-                            </div>
-                        </div>
-
-
-
-
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
     </div>
+
+
 
 
 </template>
