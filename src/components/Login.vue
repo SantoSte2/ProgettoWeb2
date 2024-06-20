be5b1f43282311fca6c506cc9500320d31b8669c
SantoSte2 <stefano.santoni@studio.unibo.it>
2024-06-20 13:48:56 +0200

    refactor Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,24 @@
 <template>
-  <div class="container mx-4 px-2 border">
+  <div class="container mx-4 my-2 px-2 py-3 border">
 
     <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-          placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      <div class="container mb-3">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+            placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      <div class="container mb-3">
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
       </div>
-
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="container mt-2 text-end">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
     </form>
 
   </div>
