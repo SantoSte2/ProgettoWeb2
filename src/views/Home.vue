7bae28b0e16dc5c070100944ef177313181bffdc
SantoSte2 <stefano.santoni@studio.unibo.it>
2024-06-20 11:02:17 +0200

    refactor homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,39 @@
 <template>
 
-  <ListaLibri :listaLibri="listaLibri" />
+  <!-- <ListaLibri :listaLibri="listaLibri" /> -->
+  <CardLibri :cardLibri="cardLibri" />
   <Footer />
-  
+
 
 </template>
 
 <script setup>
 
-import ListaLibri from '../components/ListaLibri.vue';
+//import ListaLibri from '../components/ListaLibri.vue';
+import CardLibri from '../components/CardLibri.vue';
 import Footer from '@/components/Footer.vue';
 
 
 import { defineProps } from 'vue';
-import { ref, computed, onMounted} from 'vue';
+import { ref, computed, onMounted } from 'vue';
 
-const listaLibri = ref([]);
+//const listaLibri = ref([]);
+
+const cardLibri = ref([]);
 
 
 // Funzione Memoria Locale------------------------------------
 onMounted(() => {
   // nello storage locale posso salvare solo JSON quindi faccio il parse
-  const savedlistaLibri = JSON.parse(localStorage.getItem('listaLibri'));
+  const savedcardLibri = JSON.parse(localStorage.getItem('cardLibri'));
 
-  if(savedlistaLibri){// se esistono libri salvati allora ricarico quei libri nel web
-  
-    listaLibri.value = savedlistaLibri;
+  if (savedcardLibri) {// se esistono libri salvati allora ricarico quei libri nel web
+
+    cardLibri.value = savedcardLibri;
   }
 
 });
 
 </script>
 
-<style>
-
-
-
-
-</style>
+<style></style>
