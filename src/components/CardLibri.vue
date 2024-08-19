7bc3b5aa83fd1b316e7d58de95d0e29d9e7aa026
AndrePerte <andrea.pertegato@studio.unibo.it>
2024-08-19 14:35:58 +0200

    Implementata visualizzazione libri da db

--- src/components/CardLibri.vue
+++ src/components/CardLibri.vue
@@ -1,56 +1,48 @@
 <template>
-
     <div class="col-12">
-        <div class="card-group mx-4 my-2">
-
-            <div class="row">
-                <div class="card col-4" v-for="card in cardLibri" :key="card.id">
-                    <img :src="card.imgurl" class="card-img-top" :alt="'copertina ' + card.titoloCard">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ card.titoloCard }}</h5>
-                        <h5>{{ card.copieCard }}</h5>
-                        <p class="card-text">{{ card.descrizione }}</p>
-                    </div>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-6">
-                                <button class="btn btn-primary btn-sm" v-show="(card.copieCard)"
-                                    @click="prenotaCard(card.id)">PRENOTA</button>
-                            </div>
-                            <div class="col-6">
-                                <button class="btn btn-warning btn-sm" v-if="(card.copieCard) <= 0"
-                                    @click="metticodaCard">CODA</button>
-                            </div>
-
-                        </div>
-                        <div class="row">
-                            <div class="col-6">
-                                <button class="btn btn-primary btn-sm"
-                                    @click="addcopieCard(card.id)">RESTITUISCI</button>
-                            </div>
-                            <div class="col-6">
-                                <button class="btn btn-primary btn-sm" @click="cancellaCard(card.id)">CANCELLA</button>
-                            </div>
-                        </div>
-
-
-
-
-                    </div>
-
-                </div>
-
+      <div class="card-group mx-4 my-2">
+  
+        <div class="row" v-if="libri.length > 0">
+          <div class="card col-4" v-for="libro in libri" :key="libro.idLibro">
+            <img :src="libro.immagine" class="card-img-top" :alt="'copertina ' + libro.titoloCard">
+            <div class="card-body">
+              <h5 class="card-title">{{ libro.Titolo }}</h5>
+              <h5>Copie: {{ libro.numCopie }}</h5>
+              <p class="card-text">{{ libro.trama }}</p>
             </div>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-6">
+                  <button class="btn btn-primary btn-sm" v-show="(libro.numCopie > 0)"
+                    @click="prenotaCard(libro.idLibro)">PRENOTA</button>
+                </div>
+                <div class="col-6">
+                  <button class="btn btn-warning btn-sm" v-if="(libro.numCopie) <= 0"
+                    @click="metticodaCard">CODA</button>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6">
+                  <button class="btn btn-primary btn-sm"
+                    @click="addcopieCard(libro.idLibro)">RESTITUISCI</button>
+                </div>
+                <div class="col-6">
+                  <button class="btn btn-primary btn-sm" @click="cancellaCard(libro.idLibro)">CANCELLA</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div v-else>
+          <p>Caricamento in corso...</p>
+        </div>
+      </div>
     </div>
-
-
-
-
 </template>
 
 <script setup>
 
+import axios from 'axios';
 import { defineProps } from 'vue';
 import { ref, computed, onMounted } from 'vue';
 
@@ -61,10 +53,16 @@ const emit = defineEmits(['cardCancellata', 'prenotaCard', 'addcopieCard']);
 //qui definisco questa listaLibri che deve essere un array e richiesto vero
 const props = defineProps({
 
-    cardLibri: {
-        type: Array,
-        required: true,
-    },
+});
+const libri = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost/search.php'); // Sostituisci con il tuo endpoint
+    libri.value = response.data; // Aggiorna lo stato con i dati ricevuti
+  } catch (error) {
+    console.error('Errore durante il caricamento dei libri:', error);
+  }
 });
 
 // definisco la funzione di cancellazione della Card
