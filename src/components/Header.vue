1a821a7627a3ba87268ec118ae37ea39b9af86ff
SantoSte2 <stefano.santoni@studio.unibo.it>
2024-09-11 09:32:49 +0200

    solve title Biblo on centre

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="container-fluid px-2">
         <div class="row">
-            <div class="col-5"></div>
-            <div class="col-2">
-                <h1>BIBLIO</h1>
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="text-center">
+                    <h1>BIBLIO</h1>
+                </div>
             </div>
-            <div class="col-5"></div>
+            <div class="col-3"></div>
         </div>
 
     </div>
